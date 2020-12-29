1ff414a96d27a716609a08290c952ee33bf6b23e
Николай Волков <volkovnd@yandex.ru>
2020-12-29 22:50:50 +0300

    Added stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+/** @type {import("stylelint").Configuration} */
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+    "stylelint-config-prettier"
+  ]
+};
