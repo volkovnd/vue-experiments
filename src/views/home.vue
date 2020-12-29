293de33c3df03daf95d5f9b8afd0bb516778b453
volkovnd <volkovnd@yandex.ru>
2020-12-30 01:28:41 +0300

    Little changes in content

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="main">
-    <h1>Hello World!</h1>
+  <div id="home">
+    <h1>Главная!</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, quis repudiandae? Cum,
+      tempora! Quaerat molestiae, accusamus deserunt cum iure sit reprehenderit velit perspiciatis,
+      hic quam earum quos distinctio nemo exercitationem!
+    </p>
   </div>
 </template>
 
