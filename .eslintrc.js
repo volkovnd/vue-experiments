d7335106fa284fec47138efb6aa25f6081180e4b
Николай Волков <volkovnd@yandex.ru>
2020-12-29 22:55:58 +0300

    Added eslint-plugin-import with webpack config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,25 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/prettier'
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "@vue/prettier"
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  },
+  settings: {
+    "import/resolver": {
+      webpack: {
+        config: require.resolve("@vue/cli-service/webpack.config.js")
+      }
+    }
   }
-}
+};
