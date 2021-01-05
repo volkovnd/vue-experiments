c0b11b1f8f4671a6a5bf898948099efc4d83de2c
volkovnd <volkovnd@yandex.ru>
2021-01-05 21:07:49 +0300

    Added about View

---
+++ src/views/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+    <div id="about">
+      <h1>About!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi error eligendi ullam tempore
+        iusto nostrum quaerat dolorum pariatur incidunt distinctio libero eaque a, earum, alias rem
+        aliquam veritatis illo officiis.
+      </p>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import Layout from "@/layouts/default.vue";
+
+export default {
+  name: "About",
+  components: {
+    Layout
+  }
+};
+</script>
