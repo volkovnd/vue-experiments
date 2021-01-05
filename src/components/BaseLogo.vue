f8ccd98a097f5ff3e3cac9a5ad516c6eee3eb93e
volkovnd <volkovnd@yandex.ru>
2021-01-06 02:17:38 +0300

    Added Base Logo component

---
+++ src/components/BaseLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <img src="@/assets/logo.png" :alt="alt" />
+</template>
+
+<script>
+export default {
+  props: {
+    alt: {
+      type: String,
+      default: "App Logo"
+    }
+  }
+};
+</script>
