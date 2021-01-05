9e64a6582f65dfa89fb411f13180857fdbd1aeba
volkovnd <volkovnd@yandex.ru>
2021-01-05 16:04:27 +0300

    Added default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="page">
+    <div id="header">
+      <img src="@/assets/logo.png" alt="" />
+    </div>
+    <div id="nav">
+      <router-link to="/">Главная</router-link>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout"
+};
+</script>
