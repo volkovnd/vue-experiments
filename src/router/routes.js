7bc39dd7e6bbb400798820b1498544ab8ab34d8c
volkovnd <volkovnd@yandex.ru>
2021-01-05 21:09:07 +0300

    Added about route

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,11 @@ const routes = [
     component: () => import(/* webpackChunkName: "home" */ "@/views/home.vue")
   },
   {
+    path: "/about",
+    name: "about",
+    component: () => import(/* webpackChunkName: "about" */ "@/views/about.vue")
+  },
+  {
     path: "*",
     component: () => import(/* webpackChunkName: "404" */ "@/views/404.vue")
   }
