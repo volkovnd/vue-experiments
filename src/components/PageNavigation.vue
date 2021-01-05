6e864c5ae1355e6d0c98bf8834159ef7802805c5
volkovnd <volkovnd@yandex.ru>
2021-01-06 02:49:09 +0300

    Added initial version of navigation mobile menu toggler button

--- src/components/PageNavigation.vue
+++ src/components/PageNavigation.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="navbar navbar-expand-md navbar-dark bg-dark py-2">
     <div class="container">
+      <button type="button" class="navbar-toggler" @click.prevent="toggle()">
+        <span class="navbar-toggler-icon"></span>
+      </button>
       <div
         class="navbar-collapse collapse"
         :class="{
@@ -20,6 +23,11 @@ export default {
     return {
       collapsed: false
     };
+  },
+  methods: {
+    toggle() {
+      this.collapsed = !this.collapsed;
+    }
   }
 };
 </script>
