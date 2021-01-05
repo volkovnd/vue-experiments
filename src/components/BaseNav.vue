88e5470a53048b70a0812b7d48ff042acb34efc6
volkovnd <volkovnd@yandex.ru>
2021-01-06 01:57:22 +0300

    Added some base components

---
+++ src/components/BaseNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav
+    class="nav"
+    :class="{
+      'flex-column': column
+    }"
+  >
+    <slot />
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    column: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
---
+++ src/components/BaseNavLink.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-link :to="routerLinkTo" class="nav-item nav-link" v-bind="$attrs" v-on="$listeners"
+    ><slot
+  /></router-link>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    href: {
+      type: String,
+      default: ""
+    },
+    to: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    routerLinkTo() {
+      return this.href ? this.href : this.to;
+    }
+  }
+};
+</script>
