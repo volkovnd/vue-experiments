757e1e3800be2cde10972e73e26741405decf020
volkovnd <volkovnd@yandex.ru>
2021-01-06 00:55:31 +0300

    Added animation for page changing

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <component :is="layoutComponent" :key="layoutComponent.name || layoutComponent.__filename">
-      <router-view :key="$route.fullPath" />
+      <transition name="fade" mode="out-in">
+        <router-view :key="$route.fullPath" />
+      </transition>
     </component>
   </div>
 </template>
@@ -25,4 +27,18 @@ export default {
 <style lang="scss">
 @import "@/design";
 @import "~bootstrap/scss/bootstrap";
+@import "~animate.css/animate.css";
+
+.fade-enter-active {
+  animation: fadeInDown;
+}
+
+.fade-leave-active {
+  animation: fadeOutDown;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  animation-duration: 0.25s;
+}
 </style>
